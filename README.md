45222aa15fe0cde5b93d114d8d81757371ace398
Пользователь <ramazan.magomedov@mail.ru>
2026-05-02 18:17:58 +0300

    Обновил Readme файл

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # team-portfolio
 This repository store MRDev's projects for portfolio
+Идея такова:
+- Разработаем сайт-портфолио
+- сделаем пет-проекты
+- добавим в сайт портфолио ссылки на эти сайты
--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,9 @@ from flask import redirect, render_template
 @app_var.route("/")
 @app_var.route("/index")
 def index():
-    projects = {'name_of_project' : 'Сайт для пекарни'}, {'name_of_project' : 'Сайт для авиокомпании'}
+    projects = [
+    {'name_of_project' : 'Сайт для пекарни', 'link': 'http/чтото там'},
+    {'name_of_project' : 'Сайт для авиокомпании', 'link': 'http/чтото там'}
+    ]
     return render_template('index.html', title = 'Home',projects=projects)
     
